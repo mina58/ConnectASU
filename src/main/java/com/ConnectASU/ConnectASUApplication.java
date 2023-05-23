549876e62ac082994bac1bd3e0614fe1a3e15b6c
mal-elbakry18 <100995605+mal-elbakry18@users.noreply.github.com>
2023-05-23 16:16:50 +0300

    FXML Files

--- src/main/java/com/ConnectASU/ConnectASUApplication.java
+++ src/main/java/com/ConnectASU/ConnectASUApplication.java
@@ -2,38 +2,23 @@ package com.ConnectASU;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ConnectASUApplication extends Application {
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        try{
-
-        FXMLLoader fxmlLoader = new FXMLLoader ( ConnectASUApplication.class.getResource ( "hello-view.fxml" ) );
-
-            FXMLLoader fxmlLoader2 = new FXMLLoader ( ConnectASUApplication.class.getResource ( "Sign In (2).fxml" ) );
-            Parent root = FXMLLoader.load ( getClass ().getResource ( "Sign In (2).fxml" ) );
-            Scene scene = new Scene ( root);
-        //Group root = new Group ();
-       // Scene scene2 = new Scene ( root);
-        primaryStage.setTitle ( "ConnectASU" );
-
-        primaryStage.setScene ( scene );
-        primaryStage.show ();
-        }
-
-        catch (Exception e){
-            e.printStackTrace ();
-        }
-    }
 
     public static void main(String[] args) {
-        launch (args);
+        launch(args);
     }
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader= new FXMLLoader (ConnectASUApplication.class.getResource ( "log_in.fxml" ));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
